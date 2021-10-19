b4761eaaae9fe649564ea6706c8e6200875901be
s16004993 <roxane.sauzer@etu.univ-amu.fr>
2021-10-19 16:58:42 +0200

    Tache 3

---
+++ coveringtree.py
@@ -0,0 +1,31 @@
+import math
+from graph import Graph
+
+def min_cost(cost, choices):
+    mincost=math.inf
+    min=0
+    for u in choices:
+        if cost[u]<=mincost:
+            mincost=cost[u]
+            min=u
+    return min
+
+
+def prim(graph):
+    cost={}
+    parent={}
+
+    for v in graph.nodes():
+        cost[v]=math.inf
+
+    choices=graph.nodes()
+    while len(choices)!=0:
+        min= min_cost(cost,choices)
+        choices.remove(min)
+
+        for v in graph.successors(min):
+            if v in choices and graph.arc_weight((min,v))<cost[v]:
+                cost[v]= graph.arc_weight((min,v))
+                parent[v]=min
+
+    return parent
---
+++ maze.py
@@ -0,0 +1,36 @@
+from graph import Graph
+import render
+from coveringtree import prim
+
+
+class Maze:
+    def __init__(self, width, height):
+        self.width=width
+        self.height=height
+
+    def gen_graph(self):
+        graph= Graph()
+        for x in range(self.width):
+            for y in range(self.height):
+                if x<self.width-1:
+                    graph.add_arc(((x,y),(x+1,y)))
+                    graph.add_arc(((x+1,y),(x,y)))
+                if y<self.height-1:
+                    graph.add_arc(((x,y),(x, y+1)))
+                    graph.add_arc(((x,y+1),(x,y)))
+        return graph
+
+    def draw(self):
+        render.draw_tree(self.gen_graph())
+
+    def gen_maze(self):
+        parent=prim(self.gen_graph())
+        graph=Graph()
+        
+
+
+
+
+m=Maze(2,2)
+
+print(prim(m.gen_graph()))
